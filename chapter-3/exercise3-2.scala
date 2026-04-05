bfdf82e2149bf751561ee239fe22c6049abd8a76
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2026-04-05 11:12:29 -0400

    feat(chapter-3): Add drop function and tests for exercise 3-2
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- chapter-3/exercise3-2.scala
+++ chapter-3/exercise3-2.scala
@@ -1,4 +1,5 @@
 //> using dep org.scalameta::munit::1.2.4
+//> using target.scope test
 
 def tail[A](xs: List[A]): List[A] =
   xs match
@@ -14,3 +15,21 @@ class TailSuite extends munit.FunSuite:
 
   test("tail of Nil throws"):
     intercept[RuntimeException](tail(Nil))
+
+
+def drop[A](n: Int, xs: List[A]): List[A] =
+  if n <= 0 then xs
+  else
+    xs match
+      case Nil       => Nil
+      case _ :: tail => drop(n - 1, tail)
+
+class DropSuite extends munit.FunSuite:
+    test("drop 0 elements returns the original list"):
+        assertEquals(drop(0, List(1, 2, 3)), List(1, 2, 3))
+
+    test("drop 2 elements from a multi-element list"):
+        assertEquals(drop(2, List(1, 2, 3)), List(3))
+
+    test("drop more elements than the list has returns Nil"):
+        assertEquals(drop(5, List(1, 2, 3)), Nil)
